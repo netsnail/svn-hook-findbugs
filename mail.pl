e77fda3154c1aaeeeb54185f6164bf7f1bfab643
Tiger <lihucn@qq.com>
2015-08-05 10:59:22 +0800

    aa

--- mail.pl
+++ mail.pl
@@ -3,14 +3,15 @@
 use Net::SMTP_auth;
 
 my $to_address  = $ARGV[0];
-my $message = $ARGV[1];
+my $title = $ARGV[1];
+my $message = $ARGV[2]."\n\n".$ARGV[3];
 
 my $mail_user   = 'findbugs@wcce.cn';
-my $mail_pwd    = '********';
+my $mail_pwd    = '**********';
 my $mail_server = '192.168.1.240';
 
 my $from    = "From: $mail_user\n";
-my $subject = "Subject: Some Bugs in your code!\n";
+my $subject = "Subject: $title\n";
 
 $smtp = Net::SMTP_auth->new($mail_server, Timeout => 120, Debug => 0);
 $smtp->auth('LOGIN', $mail_user, $mail_pwd);
