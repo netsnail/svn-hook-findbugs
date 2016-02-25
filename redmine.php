97302fb02d3cb55d46c66e44804e13e3a07f3774
Tiger <lihucn@qq.com>
2016-02-25 11:13:09 +0800

    upgrade

--- redmine.php
+++ redmine.php
@@ -11,25 +11,25 @@ $to=$argv[2];
 $title=$argv[3];
 $content=$argv[4];
 
-$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to '.$dbhost.' mysql server');
-mysql_select_db($dbname) or die ('Error connecting to Database: '.$dbname.'');
+$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to '.$dbhost.' mysql server\n');
+mysql_select_db($dbname) or die ('Error connecting to Database: '.$dbname.'\n');
 mysql_query("set names utf8");
 
-$test = mysql_query("SELECT id,lft,rgt FROM projects WHERE identifier = '".$project."'") or die ('Table "projects" do not exist');
+$test = mysql_query("SELECT id,lft,rgt FROM projects WHERE identifier = '".$project."'") or die ('Table "projects" do not exist\n');
 $total = mysql_fetch_assoc($test);
 if ($total) {
   $p_id = $total['id'];
   $lft = $total['lft'];
   $rgt = $total['rgt'];
 
-    $test = mysql_query("SELECT id FROM users WHERE login = '".$to."'") or die ('Table "users" do not exist');
+    $test = mysql_query("SELECT id FROM users WHERE login = '".$to."'") or die ('Table "users" do not exist\n');
     $total = mysql_fetch_assoc($test);
     if ($total) {
       $id = $total['id'];
     
-      $sql = "INSERT INTO `issues` (`tracker_id`, `project_id`, `subject`, `description`, `due_date`, `category_id`, `status_id`, `assigned_to_id`, `priority_id`, `fixed_version_id`, `author_id`, `lock_version`, `created_on`, `updated_on`, `start_date`, `done_ratio`, `estimated_hours`, `parent_id`, `root_id`, `lft`, `rgt`, `is_private`, `closed_on`) VALUES ('1', '".$p_id."', '".$title."', '".$content."', now(), NULL, '1', '".$id."', '1', NULL, '1', '1', now(), now(), now(), '0', '1', NULL, '1', '".$lft."', '".$rgt."', '0', NULL)";
+      $sql = "INSERT INTO `issues` (`tracker_id`, `project_id`, `subject`, `description`, `due_date`, `category_id`, `status_id`, `assigned_to_id`, `priority_id`, `fixed_version_id`, `author_id`, `lock_version`, `created_on`, `updated_on`, `start_date`, `done_ratio`, `estimated_hours`, `parent_id`, `root_id`, `lft`, `rgt`, `is_private`, `closed_on`) VALUES ('1', '".$p_id."', '".str_replace("'","\'",$title)."', '".str.replace("'","\'",$content)."', now(), NULL, '1', '".$id."', '1', NULL, '1', '1', now(), now(), now(), '0', '1', NULL, '1', '".$lft."', '".$rgt."', '0', NULL)";
       #echo $sql;
-      mysql_query($sql) or die ('Error sql: '.$sql);
+      mysql_query($sql) or die ('Error sql: '.$sql.'\n');
     }
 }
 
